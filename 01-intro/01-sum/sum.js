bcde65eeb92704f468b63e7f7e6bba292f2aaa0d
anatoliy3787 <anatoliy3787@gmail.com>
2020-10-29 18:47:30 +0200

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,12 +1,13 @@
-function sum(a, b) {
-    try {
+var sum = (...args) => {
+    // try {
+        if (!args.length) throw TypeError('TypeError');
         return args.reduce((cur, sum) => {
-            if(typeof cur !== 'number' || typeof sum !== 'number') throw TypeError('TypeError')
+            if (args.length < 2 || typeof cur !== 'number' || typeof sum !== 'number') throw TypeError('TypeError')
             return cur + sum
-        },0);
-    } catch(e) {
-        console.warn(e)
-    }
+        }, 0);
+    // } catch (e) {
+    //     console.warn(e)
+    // }
 }
 
 module.exports = sum;
