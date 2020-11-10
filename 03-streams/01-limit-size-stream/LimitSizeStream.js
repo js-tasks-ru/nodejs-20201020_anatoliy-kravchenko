54386e61ce2f1f9e35deb4dd187d4677e97154cf
anatoliy3787 <anatoliy3787@gmail.com>
2020-11-10 13:10:39 +0200

    Task-36 streams

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,14 +1,63 @@
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
+const fs = require('fs');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+    constructor({limit, ...options}) {
+        super(options);
+        this.limit = limit || 16000;
+    }
 
-  _transform(chunk, encoding, callback) {
+    _transform(chunk, encoding, callback) {
+        const data = Buffer.from(chunk),
+            size = Buffer.byteLength(data);
 
-  }
+        console.log('chunk: ', chunk.toString())
+        console.log('chunk size', size);
+        console.log('limit: ', this.highWaterMark)
+        console.log('readebleLength:', this.readableLength)
+
+        this.limit -= size;
+
+        console.log('eventually limit: ', this.limit)
+        console.log('=====================')
+
+        if (this.limit < 0) {
+            callback(new LimitExceededError())
+        }
+        callback(null, chunk)
+    }
 }
 
 module.exports = LimitSizeStream;
+
+//============Transform=============//
+//
+// const limitStream = new LimitSizeStream({limit: 10, encoding: 'utf-8',});
+// const smile = '😀';
+
+// limitStream.write(smile)
+// limitStream.write('ጷ')
+// setTimeout(() => limitStream.write('c'), 2000);
+// limitStream.write('c');
+// limitStream.write('world');
+// limitStream.end();
+
+//============Writable==============//
+// const writebleStream = fs.createWriteStream('output.txt');
+// //
+// // writebleStream.on('data', (chunk) => {
+// //     console.log('write', chunk)
+// // });
+// //
+// // writebleStream.on('end', () => {
+// //     console.log('write end');
+// // });
+// //
+//============Readeble==============//
+// // const readebleStream = fs.createReadStream('read.txt', {highWaterMark: 1});
+// // readebleStream.on('data', (chunk) => {
+// //     console.log('read:', chunk.toString())
+// // });
+// //
+// limitStream.pipe(writebleStream);
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,13 +4,40 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.line = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.line = chunk.toString().split('').reduce((accum, cur) => {
+      if (cur === os.EOL) {
+        this.push(accum);
+        return '';
+      }
+      return accum.concat(cur);
+    }, this.line);
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this.line);
   }
 }
 
 module.exports = LineSplitStream;
+
+// const lineSplitStream = new LineSplitStream({encoding: 'utf-8'});
+//
+// lineSplitStream.on('data', (chunk) => {
+//   console.log('line: ', chunk);
+// });
+//
+// // lineSplitStream.write(['a', 'b', 'c', 'd', 'e', 'f'].join(os.EOL));
+// lineSplitStream.write('line1\nline2');
+// lineSplitStream.write('line3\n');
+// lineSplitStream.write('line4');
+// lineSplitStream.write('line5\n');
+// lineSplitStream.write('lin');
+// lineSplitStream.write('e6');
+
+// lineSplitStream.write('world');
+// lineSplitStream.end();
